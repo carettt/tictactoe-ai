2bcff574850d7b2cf3f2f2b8c54f1b0761872407
dieb <melodyp0nd@protonmail.com>
2020-10-18 16:57:03 -0500

    Basic express.js server with POST requests

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
+app.use(express.json({ limit: '1mb' }));
+app.post('/api', (req, res) => {
+    console.log('New request!');
+    console.log(req.body);
+    const data = req.body;
+    res.json({
+        status: 'success',
+        num1: data.testNum1,
+        num2: data.testNum2
+    });
+});
