e27b8b0708cd043a63a8494299308ded161f20df
dieb <62118138+m3lodyp0nd@users.noreply.github.com>
2020-10-19 08:45:26 -0500

    Update game.py to use single quotes only
    format game.py to use uniform single quotes

--- game.py
+++ game.py
@@ -3,11 +3,11 @@ board = [0, 1, 2,
          6, 7, 8]
 
 def displayBoard():
-    print(board[0], "|", board[1], "|", board[2])
+    print(board[0], '|', board[1], '|', board[2])
     print("----------")
-    print(board[3], "|", board[4], "|", board[5])
+    print(board[3], '|', board[4], '|', board[5])
     print("----------")
-    print(board[6], "|", board[7], "|", board[8])
+    print(board[6], '|', board[7], '|', board[8])
 
 displayBoard()
 
