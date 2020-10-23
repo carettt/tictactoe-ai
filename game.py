917ef9325ecf88e7ceca0b2f2597cf86fc94e731
Diego Andrade <doctor.diebo@gmail.com>
2020-10-23 13:02:02 -0500

    Add win state

--- game.py
+++ game.py
@@ -11,26 +11,50 @@ def displayBoard():
     print('----------')
     print(board[6], '|', board[7], '|', board[8])
 
+def whoWon():
+    winStates = [['x', 'x', 'x'], ['o', 'o', 'o']]
+    boardStates = [[board[0], board[1], board[2]], [board[3], board[4], board[5]], [board[6], board[7], board[8]], [board[0], board[3], board[6]], [board[1], board[4], board[7]], [board[2], board[5], board[8]], [board[0], board[4], board[8]], [board[2], board[4], board[6]]]
+    for i in range(0, 8):
+        for j in range(0, 2):
+            if winStates[j] == boardStates[i]:
+                return [True, winStates[j]]
+    return [False, None]
+                
+
 displayBoard()
 
-while win == False:
-    try:
-        print('Player 1 pick a spot on the board: ')
-        spot = int(input())
-        if (board[spot] != 'x' and board[spot] != 'o'):
-            board[spot] = 'x'
-        else:
-            print('\nSpot is taken\n')
-    except Exception as e:
-        print('Wrong input')
-    displayBoard()
-    try:
-        print('Player 2 pick a spot on the board: ')
-        spot = int(input())
-        if (board[spot] != 'x' and board[spot] != 'o'):
-            board[spot] = 'o'
-        else:
-            print('\nSpot is taken\n')
-    except Exception as e:
-        print('Wrong input')
-    displayBoard()
+while True:
+    while True:
+        try:
+            print('Player 1 pick a spot on the board: ')
+            spot = int(input())
+            if (board[spot] != 'x' and board[spot] != 'o'):
+                board[spot] = 'x'
+                displayBoard()
+                break
+            else:
+                print('\nSpot is taken\n')
+        except Exception as e:
+            print('Wrong input')
+            displayBoard()
+    winner = whoWon()
+    if winner[0]:
+        print(winner[1][0], 'won!')
+        break
+    while True:
+        try:
+            print('Player 2 pick a spot on the board: ')
+            spot = int(input())
+            if (board[spot] != 'x' and board[spot] != 'o'):
+                board[spot] = 'o'
+                displayBoard()
+                break
+            else:
+                print('\nSpot is taken\n')
+        except Exception as e:
+            print('Wrong input')
+            displayBoard()
+    winner = whoWon()
+    if winner[0]:
+        print(winner[1], 'won!')
+        break
